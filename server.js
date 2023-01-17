3a2dc7c97e808ca24b7f0431cb2eb9b4f1e1082c
eRobda <51453874+eRobda@users.noreply.github.com>
2023-01-17 17:28:55 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -17,40 +17,42 @@ app.get('/', async (req, res) => {
   const query = req.query.arg;
 
   if(query){
-    console.log('hledám film: ' + query);
-    // Use Selenium to search Google
+    console.log(Cas() + ' Hledám film: ' + query);
+
     var options = new chrome.Options();
     options.addArguments("--headless");
     //uprava zde, v případě chyby smazat!
     options.excludeSwitches('enable-logging');
     
     let driver = await new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(options)
-    .build();
+      .forBrowser('chrome')
+      .setChromeOptions(options)
+      .build();
     try {
       //dalsi uprava zde. Original: await driver.get('https://prehrajto.cz/);
-    await driver.get('https://prehrajto.cz/hledej/' + String(query));
-    //await driver.findElement(By.id('search-phrase')).sendKeys(String(query), Key.RETURN);
+      await driver.get('https://prehrajto.cz/hledej/' + String(query));
+      //await driver.findElement(By.id('search-phrase')).sendKeys(String(query), Key.RETURN);
 
 
-    const elements = await driver.findElements(By.className('column'));
+      const elements = await driver.findElements(By.className('column'));
 
-    if(elements.length > 3){
-      nalezeno = true;
-    }else{
-      nalezeno = false;
-      console.log('nenalezeno');
-    }
+      if(elements.length > 3){
+        nalezeno = true;
+        console.log(Cas() + ' Film ' + query + ' nalezen');
+      }
+      else{
+        nalezeno = false;
+        console.log(Cas() + ' Film ' + query + ' nenalezen');
+      }
 
-    if(nalezeno){
-      const element = await driver.findElement(By.className('column'));
-      await element.click();
-      odkaz = await driver.findElement(By.className('jw-video')).getAttribute('src');
-    }
+      if(nalezeno){
+        const element = await driver.findElement(By.className('column'));
+        await element.click();
+        odkaz = await driver.findElement(By.className('jw-video')).getAttribute('src');
+      }
     }
     finally {
-    await driver.quit();;
+      await driver.quit();
     }
   }
 
@@ -65,5 +67,16 @@ app.get('/', async (req, res) => {
 });
 
 const server = app.listen(port, hostname, () => {
-  console.log(`Server bezi na://${hostname}:${port}/`);
+  console.log(Cas() + ` Server běží na: ${hostname}:${port}/`);
 });
+
+function Cas(){
+  var currentdate = new Date(); 
+  var datetime =    currentdate.getDate() + "/"
+                  + (currentdate.getMonth()+1)  + "/" 
+                  + currentdate.getFullYear() + " "  
+                  + currentdate.getHours() + ":"  
+                  + currentdate.getMinutes() + ":" 
+                  + currentdate.getSeconds();
+  return datetime;
+}
